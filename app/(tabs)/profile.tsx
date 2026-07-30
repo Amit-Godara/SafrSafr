0e00e7369633134af736c66872bc017b622372e5
Amit-Godara <mituamitgodara@gmail.com>
2026-07-30 11:35:26 +0530

    feat(profile) implemented setting page of profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -7,9 +7,15 @@ export default function ProfileTab() {
 
   return (
     <ProfileScreen
+<<<<<<< HEAD
       onMyTrips={() => {}} // TODO: build My Trips screen
       onEmergencyContacts={() => {}} // TODO: build Emergency Contacts screen
       onSettings={() => {}} // TODO: build Settings screen
+=======
+      onMyTrips={() => router.push('/my-trips')}
+      onEmergencyContacts={() => router.push('/emergency-contacts')}
+      onSettings={() => router.push('/settings')}
+>>>>>>> 60a96b4 (feat(profile) implemented setting page of profile)
       onPrivacy={() => {}} // TODO: build Privacy screen
       onHelp={() => {}} // TODO: build Help screen
       onLogout={() => router.replace('/')}
--- screens/index.ts
+++ screens/index.ts
@@ -10,4 +10,10 @@ export * from './ChatListScreen';
 export * from './PersonChatScreen';
 export * from './RoutePlannerScreen';
 export * from './NavigationScreen';
+<<<<<<< HEAD
 export * from './ProfileScreen';
+=======
+export * from './ProfileScreen';
+export * from './EmergencyContactsScreen';
+export * from './SettingsScreen';
+>>>>>>> 60a96b4 (feat(profile) implemented setting page of profile)
