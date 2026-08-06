025a44c7683640ede7622ed6a38a323edf3715c3
Amit-Godara <mituamitgodara@gmail.com>
2026-08-06 10:07:01 +0530

    some changes to _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -50,14 +50,7 @@ export default function RootLayout() {
               <Stack.Screen name="help" options={{ animation: 'slide_from_right' }} />
               <Stack.Screen name="notifications" options={{ animation: 'slide_from_right' }} />
               <Stack.Screen name="old-notifications" options={{ animation: 'slide_from_right' }} />
-<<<<<<< HEAD
               <Stack.Screen name="conversation/[id]" options={{ animation: 'slide_from_right' }} />
-=======
-              <Stack.Screen
-                name="conversation/[id]"
-                options={{ animation: 'slide_from_right' }}
-              />
->>>>>>> feature/profile
             </Stack>
           </NotificationsProvider>
         </TravelersProvider>
